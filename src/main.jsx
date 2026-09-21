da050b1b1a056f3fc9836570ec0b200d554c2c6d
khulsamiksha <khulsamiksha@gmail.com>
2026-09-21 08:26:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -217,7 +217,7 @@ function App() {
 
     try {
       const response = await fetch(
-        "http://127.0.0.1:8000/api/career/generate-plan",
+        "https://agentic-ai-hackathon-2026.onrender.com/api/career/generate-plan",
         {
           method: "POST",
           headers: {
